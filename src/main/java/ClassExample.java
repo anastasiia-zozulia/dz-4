567b7af298af7f67f30eb6187908adb1a8305bf5
Anastasiia Zozulia <rrroms321@gmail.com>
2023-06-17 23:42:08 +0300

    Completed Homework

---
+++ src/main/java/ClassExample.java
@@ -0,0 +1,7 @@
+public class ClassExample {
+    public static void main (String[] argv){
+        ClassNext next = new ClassNext();
+        System.out.println(next.getInt());
+        System.out.println(next.intExample);
+    }
+}
---
+++ src/main/java/ClassNext.java
@@ -0,0 +1,8 @@
+public class ClassNext {
+    int intExample = 10;
+
+    String getInt(){
+        return ("int value is: " + intExample);
+    }
+
+}
